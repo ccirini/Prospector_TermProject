bd6a4af264959a4f78824147f47076eeed024ec6
Cao-Tony <tonycao628@gmail.com>
2021-03-08 19:06:41 -0800

    connect server to DB

--- application/back_end/server.js
+++ application/back_end/server.js
@@ -2,4 +2,20 @@ const app = require('./express');
 
 const PORT = process.env.PORT || 5000; 
 
+require('dotenv').config();
+
+// DB set up
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.dbHOST,
+    user: process.env.dbUSER,
+    password: process.env.dbPASS,
+    database: process.env.dbNAME
+});
+
+connection.connect(err => {
+    if(err) console.log(err); 
+    else console.log("Successful connection to database")
+});
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
