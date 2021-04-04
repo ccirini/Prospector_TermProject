d10647b40c6113f674b3cd2bac79dedd8e836b9b
cao-tony <tonycao628@gmail.com>
2021-04-04 15:28:06 -0700

    update student resume route

--- application/back_end/routes/upload.routes.js
+++ application/back_end/routes/upload.routes.js
@@ -1,11 +1,13 @@
 const sql = require("../models/db.js");
 
 module.exports = app => {
-    const uploadController = require("../controllers/uploads.controller");
+    const uploadController = require("../controllers/uploadResume.controller");
+    const updateController = require("../controllers/updateResume.controller");
     const upload = require("../middleware/uploads");
 
     // create a new professor account 
     app.post("/upload", upload.single("resume"), uploadController.uploadFiles);
+
     app.get("/resume", (req, res) => {
         const { userId } = req.body;
 
@@ -27,4 +29,6 @@ module.exports = app => {
             }
         })        
     })
+    
+    app.post("/update/resume", upload.single("resume"), updateController.updateFiles);
 }
