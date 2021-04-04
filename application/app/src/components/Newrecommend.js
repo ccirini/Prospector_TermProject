c024758af73d8ab4da2d14138f6edd760af64162
fjama <faisajama99@gmail.com>
2021-04-04 09:17:02 -0700

    added 1-5 rating function

--- application/app/src/components/Newrecommend.js
+++ application/app/src/components/Newrecommend.js
@@ -1,13 +1,17 @@
 import {Link} from "react-router-dom";
-import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import React, { useState } from 'react'
+import ReactRating from 'react-rate-thing'
+
 import "./Design.css";
 
 const NewRecommend = () =>{
+    const [rate, selectRate] = useState(0);
+
     return(
         <div className="newRecommend">
             <h1 > Student Recommendation</h1>
@@ -25,7 +29,14 @@ const NewRecommend = () =>{
                 <Form.Label>Student Recommendation / Letter of Recommendation</Form.Label>
                 <Form.Control as="textarea" rows={10} />
             </Form.Group> 
-    
+
+            <div style={{ margin: '1em' }}>
+                <div style={{ marginTop: '1em' }}>
+                        Student Overall Rating: {rate}
+                </div>
+                <ReactRating value={rate} onClick={selectRate} />
+            </div>
+
             <Form>
                 <Button href="/viewrecommend">Submit</Button>
             </Form>
