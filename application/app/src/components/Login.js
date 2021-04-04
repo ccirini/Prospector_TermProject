fe473530110254c2ac2bad11af6f374f99031145
franklinarevalo <franklinarevalo12@gmail.com>
2021-04-04 16:21:08 -0700

    added the other categories for homeRecruiter

--- application/app/src/components/Login.js
+++ application/app/src/components/Login.js
@@ -1,10 +1,10 @@
 import "./Login.css"
 import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Login = () =>{
     // Get login and check if it's in db
--- application/back_end/config/db.config.js
+++ application/back_end/config/db.config.js
@@ -1,8 +1,22 @@
+// module.exports = {
+//     dbHOST: "localhost",
+//     dbNAME: "csc648Team2",
+//     dbUSER: "ubuntu",
+//     dbPASS: "Csc648Team2!",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
+
 module.exports = {
     dbHOST: "localhost",
     dbNAME: "csc648Team2",
-    dbUSER: "ubuntu",
-    dbPASS: "Csc648Team2!",
+    dbUSER: "root",
+    dbPASS: "",
     dialect: "mysql",
     pool: {
         max: 5,
