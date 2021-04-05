fe803a99ea7d8d2d36d270c394caea391a43db3a
fjama <faisajama99@gmail.com>
2021-04-05 03:06:57 -0700

    added colors to button

--- application/app/src/pages/professor/Home-Professor.js
+++ application/app/src/pages/professor/Home-Professor.js
@@ -1,20 +1,27 @@
+import "./Home-Professor.css"
+import {Link} from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
-import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const HomeProfessor = () =>{
     return(
         <div className="home-professor">
-            <h1>Recommend Students!</h1>
+            <h1 className="home-professor-h1">Recommend Students!</h1>
             <InputGroup className="home-professor-input">
-                <FormControl aria-describedby="basic-addon1" placeholder="Search Student ID"
-                className="search"/>
-                <Button type="submit" className="home-professor-btn">Search</Button>
+                <FormControl aria-describedby="basic-addon1" placeholder="Search Student ID" className="home-professor-search"/>
+                <Button type="submit" className="home-professor-btn1">Search</Button>
+                <Button href="/viewrecommend" variant="outline-dark" className="home-professor-btn2">View Recommedations</Button>
             </InputGroup>
-            <Form>
-                <Button href="/viewrecommend" variant="outline-dark" className="home-professor-btn">View Recommedations</Button>
-            </Form>
+
+            <Container className="home-professor-container">
+                <h2 className="home-professor-h2">Student Name</h2>
+                <Link to="#" className="home-professor-link">View Student Portal</Link>
+                    <Button href="/newrecommend" variant="outline-dark" className="home-professor-btn3">Recommed</Button>
+            </Container>
         </div>
     );
 }
