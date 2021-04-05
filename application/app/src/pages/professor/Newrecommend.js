b7da2078b247853680aed652df4a649be97652a8
fjama <faisajama99@gmail.com>
2021-04-05 04:31:08 -0700

    styled the form with the 1-5 rating

--- application/app/src/pages/professor/Newrecommend.js
+++ application/app/src/pages/professor/Newrecommend.js
@@ -1,3 +1,4 @@
+import "./Newrecommend.css";
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
@@ -6,39 +7,36 @@ import Col from 'react-bootstrap/Col'
 import React, { useState } from 'react'
 import ReactRating from 'react-rate-thing'
 
-import "../Design.css";
-
 const NewRecommend = () =>{
     const [rate, selectRate] = useState(0);
 
     return(
         <div className="newRecommend">
-            <h1 > Student Recommendation</h1>
-            <Container class="text-color">
+            <h1 className="newRecommend-h1"> Student Recommendation</h1>
+            <Container  className="newRecommend-container">
                 <Row>
-                    <Col>Student Name:</Col>
+                    <Col className="newRecommend-font">Student Name: John Doe</Col>
+                </Row>
+                <Row>
+                    <Col className="newRecommend-font">Student ID: 123456789</Col>
                 </Row>
 
-                <Row>
-                    <Col>Student ID:</Col>
-                </Row>
-            </Container>
-
-            <Form.Group controlId="Form.Recommend">
+            <Form.Group controlId="Form.Recommend" className="newRecommend-form">
                 <Form.Label>Student Recommendation / Letter of Recommendation</Form.Label>
                 <Form.Control as="textarea" rows={10} />
             </Form.Group> 
 
-            <div style={{ margin: '1em' }}>
-                <div style={{ marginTop: '1em' }}>
+            <div className="newRecommend-rating" style={{ margin: '3em'}}>
+                <div className="newRecommend-rating-font" style={{ marginTop: '1em' }}>
                         Student Overall Rating: {rate}
                 </div>
-                <ReactRating value={rate} onClick={selectRate} />
+                <ReactRating value={rate} onClick={selectRate}/>
             </div>
 
             <Form>
-                <Button href="/viewrecommend">Submit</Button>
+                <Button href="/viewrecommend" className="newRecommend-btn">Submit</Button>
             </Form>
+            </Container>
         </div>
 
     );
