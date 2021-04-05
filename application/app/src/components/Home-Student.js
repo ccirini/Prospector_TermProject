01296a05b55ecd4e91ebdcca0182a3267cd63104
fjama <faisajama99@gmail.com>
2021-04-04 17:15:11 -0700

    fully designed account pages

---
+++ application/app/src/components/Home-Student.js
@@ -0,0 +1,13 @@
+import "./Design.css"
+import {Link} from "react-router-dom";
+
+
+const HomeStudent = () =>{
+    return(
+        <div className="home-student">
+            <h1>Matches</h1>
+        </div>
+    );
+}
+
+export default HomeStudent;
