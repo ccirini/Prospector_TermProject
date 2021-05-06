b024653b563161b0ad82b234a610bf35f6949508
fjama <faisajama99@gmail.com>
2021-05-06 15:21:24 -0700

    added search function to professor's home

--- application/app/src/pages/professor/Home-Professor.js
+++ application/app/src/pages/professor/Home-Professor.js
@@ -1,6 +1,11 @@
 import "./Home-Professor.css"
 import { Link } from "react-router-dom";
 import ProfessorNavbar from "../../components/ProfessorNavbar";
+
+import React, { useState } from 'react'
+import Select from 'react-select'
+import axios from 'axios'
+
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
@@ -8,22 +13,82 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+import API_BASE from '../config/config'
+
 const HomeProfessor = () => {
+
+	const [selectedValue, setSelectedValue] = useState('default');
+	const [searchValue, setSearchValue] = useState('default');
+	const [active, setActive] = useState(false);
+	const [students, setStudents] = React.useState([]);
+	const [stuId, setStuId] = useState(912345670);
+	const [pdfLink, setPdfLink] = useState('./john-doe.pdf');
+
+
+	const handleClick = (e) => {
+		e.preventDefault();
+		console.log('The button was clicked.');
+		setActive(true); // results will now be visible
+
+		if (selectedValue != "default" && searchValue == "default") {
+			// if dropdown is selected and search is empty
+			alert("Search bar is empty!");
+		} else {
+			axios.get(`${API_BASE}/search/${selectedValue}/text/${searchValue}`)
+				.then(response => {
+					setStudents(response.data)
+					console.log(response)
+				})
+				.catch(error => {
+					console.log(error)
+				});
+
+			console.log(students);
+		}
+	}
+
 	return (
 		<div className="home-professor">
 			<ProfessorNavbar />
-			<h1 className="home-professor-h1">Recommend Students!</h1>
-			<InputGroup className="home-professor-input">
-				<FormControl aria-describedby="basic-addon1" placeholder="Search Student ID" className="home-professor-search" />
-				<Button type="submit" className="home-professor-btn1">Search</Button>
-				<Button href="/viewrecommend" variant="outline-dark" className="home-professor-btn2">View Recommedations</Button>
-			</InputGroup>
-
-			<Container className="home-professor-container">
+			<Container>
+				<h1 className="home-professor-h1">Recommend Students!</h1>
+				<Row className="home-professor-row">
+					<InputGroup className="home-professor-input">
+					<Col className="home-professor-col">
+						<FormControl aria-describedby="basic-addon1" placeholder="Search Student ID" 
+							className="home-professor-search" onChange={e => setSearchValue(e.target.value)} />
+					</Col>		
+						<div>
+							<Button type="submit" className="home-professor-btn1" onClick={handleClick}>Search</Button>
+						</div>
+						<div>
+							<Button href="/viewrecommend" variant="outline-dark" className="home-professor-btn2">View Recommedations</Button>
+						</div>
+					</InputGroup>
+				</Row>
+			</Container>	
+			
+			{/* <Container className="home-professor-container">
 				<h2 className="home-professor-h2">Student Name</h2>
 				<Link to="#" className="home-professor-link">View Student Portal</Link>
 				<Button href="/newrecommend" variant="outline-dark" className="home-professor-btn3">Recommend</Button>
-			</Container>
+			</Container> */}
+
+			{active && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+				{students.map(student =>
+					<Container key={student.userId} className="outter-result-container">
+						<Container className="result-container">
+							<div className="row-1">
+								<p clasName="student-name"><b>{student.firstName} {student.lastName} </b></p>
+								<Button href="/newrecommend" variant="outline-dark" className="home-professor-btn3">Recommend</Button>
+							</div>
+							<div className="row-2">
+								<p className="student-major"><b>Major -</b> {student.major}</p>
+								<Button href={`${API_BASE}/resume?userId=${student.userId}`}>Download Resume</Button>
+							</div>
+						</Container>
+					</Container>)}
+			</div>}
 		</div>
 	);
 }
