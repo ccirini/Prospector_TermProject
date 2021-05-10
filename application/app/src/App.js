9bc28cf24c2e9a8e8796eec6a23e641925c85138
ccirini <70170016+ccirini@users.noreply.github.com>
2021-05-10 00:18:57 -0700

    Update App.js
    tracking-id updated to UA

--- application/app/src/App.js
+++ application/app/src/App.js
@@ -39,7 +39,7 @@ import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from 'react';
 
-ReactGA.initialize('G-XE855YGNH9'); // add your tracking id here.
+ReactGA.initialize('UA-195435223-1'); // add your tracking id here.
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
