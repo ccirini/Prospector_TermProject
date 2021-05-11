51b096fb83d80f18197a7e5516348bb21aac1841
franklinarevalo <franklinarevalo12@gmail.com>
2021-05-10 22:14:31 -0700

    signup for student is now working

--- application/back_end/config/db.config.js
+++ application/back_end/config/db.config.js
@@ -1,24 +1,9 @@
 // SERVER DB CREDENTIALS 
-// module.exports = {
-//     dbHOST: "localhost",
-//     dbNAME: "csc648Team2",
-//     dbUSER: "ubuntu",
-//     dbPASS: "Csc648Team2!",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// }
-
-// // FRONT_END DB CREDENTIALS 
 module.exports = {
     dbHOST: "localhost",
     dbNAME: "csc648Team2",
-    dbUSER: "root",
-    dbPASS: "",
+    dbUSER: "ubuntu",
+    dbPASS: "Csc648Team2!",
     dialect: "mysql",
     pool: {
         max: 5,
@@ -27,6 +12,21 @@ module.exports = {
         idle: 10000
     }
 }
+
+// // FRONT_END DB CREDENTIALS 
+// module.exports = {
+//     dbHOST: "localhost",
+//     dbNAME: "csc648Team2",
+//     dbUSER: "root",
+//     dbPASS: "",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
 
 // BACK_END DB CREDENTIALS 
 // module.exports = {
